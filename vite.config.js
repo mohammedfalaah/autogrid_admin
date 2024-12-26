30b8939eb56946cef622a972771f724928eef6d1
Muhammed faiz kk <fayizkk5@gmail.com>
2024-12-26 23:24:29 +0530

    addedetitconfig

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // needed for the Docker Container port mapping to work
-    port: 3007, // you can replace this port with any port
+    port: 3007, 
   },
-
+  base: '/autogrid_admin/',
 });
-
