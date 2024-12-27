2c2bdc0c353071bc8b26aada731eab0829e453db
mohammedfalaah <falahmkba@gmail.com>
2024-12-27 12:10:54 +0530

    layout update

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -8,9 +8,64 @@ import { Toaster } from 'react-hot-toast'
 import { contextData } from '../services/Context'
 
 const Layout = () => {
-  const { closeSidebar } = useContext(contextData);
+  const {openSidebar}=useContext(contextData)
   return (
     <>
+      <Helmet>
+    <script type="module" src="/src/main.jsx"></script>
+    <script src="/assets/js/plugins/apexcharts.min.js"></script>
+    <script src="/assets/js/plugins/jsvectormap.min.js"></script>
+    <script src="/assets/js/plugins/world.js"></script>
+    <script src="/assets/js/plugins/world-merc.js"></script>
+    <script src="/assets/js/pages/dashboard-default.js"></script>
+    <script src="/assets/js/plugins/popper.min.js"></script>
+    <script src="/assets/js/plugins/simplebar.min.js"></script>
+    <script src="/assets/js/plugins/bootstrap.min.js"></script>
+    <script src="/assets/js/fonts/custom-font.js"></script>
+    <script src="/assets/js/pcoded.js"></script>
+    <script src="/assets/js/plugins/feather.min.js"></script>
+
+
+    <script src="/assets/js/plugins/apexcharts.min.js"></script>
+    <script src="/assets/js/plugins/jsvectormap.min.js"></script>
+    <script src="/assets/js/plugins/world.js"></script>
+    <script src="/assets/js/plugins/world-merc.js"></script>
+    <script src="/assets/js/pages/dashboard-default.js"></script>
+    <script src="/assets/js/plugins/popper.min.js"></script>
+    <script src="/assets/js/plugins/simplebar.min.js"></script>
+    <script src="/assets/js/plugins/bootstrap.min.js"></script>
+    <script src="/assets/js/fonts/custom-font.js"></script>
+    <script src="/assets/js/pcoded.js"></script>
+    <script src="/assets/js/plugins/feather.min.js"></script>
+
+       
+    <script>
+      layout_change('light');
+    </script>
+       
+    <script>
+      layout_sidebar_change('light');
+    </script>
+      
+    <script>
+      change_box_container('false');
+    </script>
+     
+    <script>
+      layout_caption_change('true');
+    </script>
+       
+    <script>
+      layout_rtl_change('false');
+    </script>
+     
+    <script>
+      preset_change('preset-1');
+    </script>
+    
+
+
+    </Helmet>
     <div>
         <div className="loader-bg">
             <div className="loader-track">
@@ -19,7 +74,7 @@ const Layout = () => {
         </div>
         <Sidebar />
         <Header />
-        <div onClick={closeSidebar} className="pc-container">
+        <div onClick={()=>openSidebar()} className="pc-container">
                                 <div className="pc-content">
 
                 <Outlet />
