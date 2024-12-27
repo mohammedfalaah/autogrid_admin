90ef34c2f0f78272d9d2947d6d0624995cd79140
Muhammed faiz kk <fayizkk5@gmail.com>
2024-12-27 17:30:37 +0530

    changedlog

--- src/layout/components/Header.jsx
+++ src/layout/components/Header.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { contextData } from '../../services/Context';
 
 const Header = () => {
-  const { setUser } = useContext(contextData); 
+  const { setUser } = useContext(contextData);
   let navigate = useNavigate();
   const logoutHandler = () => {
     window.localStorage.clear()
@@ -11,61 +11,61 @@ const Header = () => {
     return navigate("/login")
   }
 
-  const {sidebr,toggleSidebar}=useContext(contextData)
+  const { sidebr, toggleSidebar } = useContext(contextData)
 
 
   return (
     <>
-     <header className="pc-header">
-    <div className="header-wrapper"> {/* [Mobile Media Block] start */}
-      <div className="me-auto pc-mob-drp">
-      <ul className="list-unstyled">
-          {/* ======= Menu collapse Icon ===== */}
-          <li onClick={()=>toggleSidebar()}  className="pc-h-item pc-sidebar-collapse">
-            <a  className="pc-head-link ms-0" id="sidebar-hide">
-              <i className="ti ti-menu-2" />
-            </a>
-          </li>
-          <li onClick={()=>toggleSidebar()} className="pc-h-item pc-sidebar-popup">
-            <a href="#" className="pc-head-link ms-0" id="mobile-collapse">
-              <i className="ti ti-menu-2" />
-            </a>
-          </li>
-          <li className="dropdown pc-h-item d-inline-flex d-md-none">
-            <a className="pc-head-link dropdown-toggle arrow-none m-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-              <i className="ph-duotone ph-magnifying-glass" />
-            </a>
-            <div className="dropdown-menu pc-h-dropdown drp-search">
-              <form className="px-3">
-                <div className="mb-0 d-flex align-items-center">
-                  <input type="search" className="form-control border-0 shadow-none" placeholder="Search..." />
-                  <button className="btn btn-light-secondary btn-search">Search</button>
+      <header className="pc-header">
+        <div className="header-wrapper"> {/* [Mobile Media Block] start */}
+          <div className="me-auto pc-mob-drp">
+            <ul className="list-unstyled">
+              {/* ======= Menu collapse Icon ===== */}
+              <li onClick={() => toggleSidebar()} className="pc-h-item pc-sidebar-collapse">
+                <a className="pc-head-link ms-0" id="sidebar-hide">
+                  <i className="ti ti-menu-2" />
+                </a>
+              </li>
+              <li onClick={() => toggleSidebar()} className="pc-h-item pc-sidebar-popup">
+                <a href="#" className="pc-head-link ms-0" id="mobile-collapse">
+                  <i className="ti ti-menu-2" />
+                </a>
+              </li>
+              <li className="dropdown pc-h-item d-inline-flex d-md-none">
+                <a className="pc-head-link dropdown-toggle arrow-none m-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                  <i className="ph-duotone ph-magnifying-glass" />
+                </a>
+                <div className="dropdown-menu pc-h-dropdown drp-search">
+                  <form className="px-3">
+                    <div className="mb-0 d-flex align-items-center">
+                      <input type="search" className="form-control border-0 shadow-none" placeholder="Search..." />
+                      <button className="btn btn-light-secondary btn-search">Search</button>
+                    </div>
+                  </form>
                 </div>
-              </form>
-            </div>
-          </li>
-          <li className="pc-h-item d-none d-md-inline-flex">
-           
-          </li>
-        </ul>
-      </div>
-      {/* [Mobile Media Block end] */}
-      <div className="ms-auto">
-        <ul className="list-unstyled">
-       
-          
-        
-        
-         
-          <li className="dropdown pc-h-item header-user-profile">
-            <a onClick={logoutHandler} className="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" data-bs-auto-close="outside" aria-expanded="false">
-Logout            </a>
-          
-          </li>
-        </ul>
-      </div>
-    </div>
-  </header>
+              </li>
+              <li className="pc-h-item d-none d-md-inline-flex">
+
+              </li>
+            </ul>
+          </div>
+          {/* [Mobile Media Block end] */}
+          <div className="ms-auto">
+            <ul className="list-unstyled">
+
+
+
+
+
+              <li className="dropdown pc-h-item header-user-profile">
+                <a onClick={logoutHandler} className="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" data-bs-auto-close="outside" aria-expanded="false">
+                  Logout            </a>
+
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
 
     </>
   )
--- src/layout/components/Sidebar.jsx
+++ src/layout/components/Sidebar.jsx
@@ -1,80 +1,67 @@
-import React, {  useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { contextData } from '../../services/Context'
 import { Link } from 'react-router-dom'
-import {  basePath, dashboardPath, ordersPath, productPath } from '../../utils/Constants'
+import { basePath, dashboardPath, ordersPath, productPath } from '../../utils/Constants'
 
 const Sidebar = () => {
   const { sidebr, toggleSidebar } = useContext(contextData)
-  
+
+  // Function to handle sidebar toggle and close when a menu item is clicked
+  const handleMenuClick = () => {
+    if (sidebr) {
+      toggleSidebar(); // Close the sidebar if it's open
+    }
+  }
 
   return (
     <>
-    
-    <nav  className={`pc-sidebar ${sidebr?"mob-sidebar-active":"pc-sidebar-hide mob-sidebar-active"}`}>
-    <div className="navbar-wrapper">
-      <div className="m-header">
-        <Link to={basePath} className="b-brand text-primary">
-          <img style={{width:'100px',height:'25px'}}  src="/assets/images/AGI.png" alt="logo image" className="logo-lg" />
-        </Link>
-        <h5 onClick={()=>toggleSidebar()} style={{marginLeft:"180px"}}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></h5>
-
-      </div>
-      <div className="navbar-content">
-        <ul className="pc-navbar">
-         
-          <li className="pc-item pc-hasmenu">
-            <Link to={basePath} className="pc-link">
-              <span className="pc-micon">
-                <i className="ph-duotone ph-gauge" />
-              </span>
-              <span className="pc-mtext">Dashboard</span>
-             
+      <nav className={`pc-sidebar ${sidebr ? "mob-sidebar-active" : "pc-sidebar-hide mob-sidebar-active"}`}>
+        <div className="navbar-wrapper">
+          <div className="m-header">
+            <Link to={basePath} className="b-brand text-primary">
+              <img style={{ width: '100px', height: '25px' }} src="/AGI-LOGO.png" alt="logo image" className="logo-lg" />
             </Link>
-          
-          </li>
-    
-        
-          <li className="pc-item pc-hasmenu">
-            <Link  to={productPath} className="pc-link">
-              <span className="pc-micon">
-                <i className="ph-duotone ph-shopping-cart" />
-              </span>
-              <span className="pc-mtext">Products</span><span className="pc-arrow"><i data-feather="chevron-right" /></span></Link>
-         
-          </li>
-          <li className="pc-item pc-hasmenu">
-            <Link  to={ordersPath} className="pc-link">
-              <span className="pc-micon">
-                <i className="ph-duotone ph-shopping-bag" />
-              </span>
-              <span className="pc-mtext">Orders</span><span className="pc-arrow"><i data-feather="chevron-right" /></span></Link>
-         
-          </li>
-        
-        
-         
-      
-    
-         
-         
-        
-      
-     
-         
-         
-        
-        
-      
-           
+            <h5 onClick={() => toggleSidebar()} style={{ marginLeft: "180px" }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x">
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+              </svg>
+            </h5>
+          </div>
+          <div className="navbar-content">
+            <ul className="pc-navbar">
+              <li className="pc-item pc-hasmenu">
+                <Link to={dashboardPath} className="pc-link" onClick={handleMenuClick}>
+                  <span className="pc-micon">
+                    <i className="ph-duotone ph-gauge" />
+                  </span>
+                  <span className="pc-mtext">Dashboard</span>
+                </Link>
+              </li>
 
-         
-        </ul>
-     
-      </div>
-    
-    </div>
-  </nav>
+              <li className="pc-item pc-hasmenu">
+                <Link to={productPath} className="pc-link" onClick={handleMenuClick}>
+                  <span className="pc-micon">
+                    <i className="ph-duotone ph-shopping-cart" />
+                  </span>
+                  <span className="pc-mtext">Products</span>
+                  <span className="pc-arrow"><i data-feather="chevron-right" /></span>
+                </Link>
+              </li>
 
+              <li className="pc-item pc-hasmenu">
+                <Link to={ordersPath} className="pc-link" onClick={handleMenuClick}>
+                  <span className="pc-micon">
+                    <i className="ph-duotone ph-shopping-bag" />
+                  </span>
+                  <span className="pc-mtext">Orders</span>
+                  <span className="pc-arrow"><i data-feather="chevron-right" /></span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
